4dca3797f70a94460be6c3e25c8754276545022f
Kingstar8884 <115646493+Kingstar8884@users.noreply.github.com>
2024-07-21 22:14:58 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,30 @@ if(count == null){
     localStorage.setItem('count' , '1')
 }
 
+
+
+        // Prevent pinch-to-zoom
+        document.addEventListener('gesturestart', function (e) {
+            e.preventDefault();
+        });
+
+        // Prevent double-tap zoom
+        let lastTouchEnd = 0;
+        document.addEventListener('touchend', function (e) {
+            const now = (new Date()).getTime();
+            if (now - lastTouchEnd <= 300) {
+                e.preventDefault();
+            }
+            lastTouchEnd = now;
+        }, false);
+
+        // Prevent scale changes
+        document.addEventListener('touchmove', function (e) {
+            if (e.scale !== 1) {
+                e.preventDefault();
+            }
+        }, { passive: false });
+
 image.addEventListener('click' , (e)=> {
 
     //let x = e.offsetX;
